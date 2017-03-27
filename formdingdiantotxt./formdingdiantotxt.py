052cd84736e46964e085511d2f0dc1dc7645bb34
BARKTEGH <BARKTEGH@users.noreply.github.com>
2017-03-27 18:50:42 +0800

    Update formdingdiantotxt.py
    改进输入的小说名与搜索到的小说不符合情况下，结束程序

--- formdingdiantotxt./formdingdiantotxt.py
+++ formdingdiantotxt./formdingdiantotxt.py
@@ -22,7 +22,7 @@ class xiaoshuodownload(object):
         response = requests.post(url,proxies= proxies)
         return response.content
 
-    #通过小说名得到该小说的index界面
+    #通过小说名得到该小说的index界面,是找到搜索界面的搜索到第一个小说
     def get_xiaoshuoURL(self):
         queryurl ='http://so.32xs.com/cse/search?q='+ self.novelname.encode('utf8')\
                   +'&click=1&entry=1&s=10214632002682711881&nsid='
@@ -30,9 +30,10 @@ class xiaoshuodownload(object):
         resopnse = self.get_page(queryurl)
         soup = BeautifulSoup(resopnse,'html.parser')
         URLl = soup.find(cpos='title').get('href')
+        novelname = soup.find(cpos='title').get('title')
         xiaoshuondexpage = URLl
         self.menu = xiaoshuondexpage.split('index')[0]
-        return xiaoshuondexpage
+        return xiaoshuondexpage,novelname
 
     def get_xiaoshuomenuURL(self,indexURL):
         menupage = self.get_page(indexURL)
@@ -55,20 +56,26 @@ class xiaoshuodownload(object):
     def run(self):
         self.novelname = raw_input('请输入小说名:').decode('utf8')
         #self.novelname = u'雪中悍刀行'
-        filename = self.novelname  + '.txt'
-        f = open(self.novelname,'w+')
-        f.write(self.novelname.encode('utf8') + '\n')
-        xiaoshuopage = self.get_xiaoshuoURL()
-        self.get_xiaoshuomenuURL(xiaoshuopage) # 得到小说章节目录的url
-        for chapterURL in self.chapterlistURL:
-            chaptercontent,chaptertitle = self.get_chaptercontent(chapterURL)
-            print chaptertitle
-            print chapterURL
-            f.write(chaptertitle + '\n')
-            f.write(chaptercontent + '\n')
-            f.write('--------------------------' +'\n')
-        f.close()
-        print '小说下载完毕'
+        xiaoshuopage,novelname1 = self.get_xiaoshuoURL()
+        if novelname1 == self.novelname:
+            print '在顶点小说网找到小说'
+            print '开始爬取小说'
+            print '小说目录界面：', xiaoshuopage
+            filename = self.novelname + '.txt'
+            f = open(self.novelname, 'w+')
+            f.write(self.novelname.encode('utf8') + '\n')
+            self.get_xiaoshuomenuURL(xiaoshuopage) # 得到小说章节目录的url
+            for chapterURL in self.chapterlistURL:
+                chaptercontent,chaptertitle = self.get_chaptercontent(chapterURL)
+                print chaptertitle
+                print chapterURL
+                f.write(chaptertitle + '\n')
+                f.write(chaptercontent + '\n')
+                f.write('--------------------------' +'\n')
+            f.close()
+            print '小说下载完毕'
+        else:
+            print '未找到该小说，程序结束'
 
 def main():
     xiaoshuo = xiaoshuodownload()
